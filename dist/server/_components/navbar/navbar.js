c0280c6b1ddb79c153907f3c4b9c4ea5b589a1bd
Tom Skelton <thomas.skelton.19@gmail.com>
2024-12-17 06:16:12 +0000

    styled navbar

--- dist/server/_components/navbar/navbar.js
+++ dist/server/_components/navbar/navbar.js
@@ -4,10 +4,12 @@ var React = require('react');
 var ReactDom = require('react-dom');
 function Navbar() {
   return /*#__PURE__*/React.createElement("div", {
-    className: "test"
+    className: "navbar"
   }, /*#__PURE__*/React.createElement("link", {
     rel: "stylesheet",
     href: "_components/navbar/style.css"
-  }), "test");
+  }), /*#__PURE__*/React.createElement("button", {
+    className: "logo"
+  }, "houzen"), /*#__PURE__*/React.createElement("button", null, "Buy"), /*#__PURE__*/React.createElement("button", null, "Sell"), /*#__PURE__*/React.createElement("button", null, "Rent"));
 }
 module.exports = Navbar;
--- src/server/_components/navbar/navbar.jsx
+++ src/server/_components/navbar/navbar.jsx
@@ -3,8 +3,9 @@ const ReactDom = require('react-dom');
 
 function Navbar() {
   return (
-    <div className='test'>
+    <div className='navbar'>
       <link rel="stylesheet" href="_components/navbar/style.css" />
+      <button className='logo'>houzen</button>
       <button>Buy</button>
       <button>Sell</button>
       <button>Rent</button>
